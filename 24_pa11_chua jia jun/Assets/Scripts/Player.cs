a75bbff71ffccc8d6923c211b743a07e8a0819be
derpy83 <jacobchua00@gmail.com>
2020-06-18 18:51:32 +0800

    updated by chua jia jun

---
+++ 24_pa11_chua jia jun/Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class Player : MonoBehaviour
+{
+    public float speed;
+    private int Score = 0;
+    public Text Txt_Score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Score = 0;
+        Txt_Score.text = "SCORE : 0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+
+      
+
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("Gameover");
+        }
+        if (other.gameObject.tag == "score")
+        {
+            Score += 1;
+            Txt_Score.text = "SCORE : " + Score;
+        }
+    }
+}
---
+++ 24_pa11_chua jia jun/Assets/Scripts/Spawnerscript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnerscript : MonoBehaviour
+{
+    public GameObject SpawnObject,SpawnObject2 ,SpawnObject3, spawnobj4;
+    float PositionY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObjects", 1, 1);
+        InvokeRepeating("SpawnObjects2", 1, 1);
+        InvokeRepeating("SpawnObjects3", 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+  
+    void SpawnObjects()
+    {
+        PositionY = Random.Range(4, -4f);
+        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+        int i = Random.Range(1, 10);
+        GameObject obj;
+        if (i > 7)
+        {
+            obj = SpawnObject;
+        }
+        else if (i > 4)
+        {
+            obj = SpawnObject2;
+        }
+        else if (i > 2)
+        {
+            obj = SpawnObject3;
+        }
+        else
+        {
+            obj = spawnobj4;
+        }
+        Instantiate(obj, transform.position, transform.rotation);
+    }
+
+}
